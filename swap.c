92ea714c726ebf02f48297c53c08459165cdb518
vandriodd <ludasaadi@gmail.com>
2023-05-17 19:27:44 -0700

    Add comment

--- swap.c
+++ swap.c
@@ -9,6 +9,7 @@ void swap(int *array, size_t integer)
 {
 	int tmp = 0;
 
+	/* array[integer] = current position, array[integer + 1] = next position */
 	tmp = array[integer];
 	array[integer] = array[integer + 1];
 	array[integer + 1] = tmp;

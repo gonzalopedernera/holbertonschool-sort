8df80f1a1a68bcb7675d79105973e627ad032dbf
vandriodd <ludasaadi@gmail.com>
2023-05-17 21:22:58 -0700

    Bubble sort prototype

--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *array, size_t integer);
+void bubble_sort(int *array, size_t size);
 
 #endif

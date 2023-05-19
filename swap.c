ad78ec7568983d5009634506bcd4b2293fb20cb5
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-19 07:27:02 -0700

    Swap function update

--- swap.c
+++ swap.c
@@ -3,14 +3,14 @@
 /**
  * swap - swaps the value of two integers
  * @array: array of integers
- * @integer: integer value to swap
+ * @a: integer value to swap
+ * @b: integer value to swap
 */
-void swap(int *array, size_t integer)
+void swap(int *array, int a, int b)
 {
 	int tmp = 0;
 
-	/* array[integer] = current position, array[integer + 1] = next position */
-	tmp = array[integer];
-	array[integer] = array[integer + 1];
-	array[integer + 1] = tmp;
+	tmp = array[a];
+	array[a] = array[b];
+	array[b] = tmp;
 }
